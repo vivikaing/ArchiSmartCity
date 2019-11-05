77fee3db567b9f3e9d266fb42956e8a0b62dd268
vivikaing <vivikaing@gmail.com>
2019-11-05 15:24:11 +0200

    Path of the image for the icon, added

--- com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/elements/DomainFigure.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/elements/DomainFigure.java
@@ -1,5 +1,6 @@
 package com.archimatetool.editor.diagram.figures.elements;
 
+import java.io.File;
 import java.io.InputStream;
 
 import org.eclipse.draw2d.ChopboxAnchor;
@@ -13,6 +14,7 @@ import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Path;
 import org.eclipse.swt.graphics.Pattern;
 import org.eclipse.swt.widgets.Display;
@@ -21,9 +23,10 @@ import com.archimatetool.editor.diagram.figures.AbstractTextControlContainerFigu
 import com.archimatetool.editor.diagram.figures.FigureUtils;
 import com.archimatetool.editor.preferences.IPreferenceConstants;
 import com.archimatetool.editor.preferences.Preferences;
+import com.archimatetool.editor.ui.IArchiImages;
+import com.archimatetool.editor.ui.ImageFactory;
 import com.archimatetool.model.IDiagramModelArchimateObject;
 import com.archimatetool.model.ITextPosition;
-
 
 /**
  * Domain Figure
@@ -31,163 +34,156 @@ import com.archimatetool.model.ITextPosition;
  * @author Viviana Bastidas
  */
 public class DomainFigure extends AbstractTextControlContainerFigure {
-	 private static final int TOPBAR_HEIGHT = 18;
-	    private static final float INSET = 1.4f;
-	    
-	    public DomainFigure() {
-	        super(TEXT_FLOW_CONTROL);
-	    }
-	   
-	    @Override
-	    protected void drawFigure(Graphics graphics) {
-	        graphics.pushState();
-	        
-	        Rectangle bounds = getBounds().getCopy();
-	        
-	        graphics.setAntialias(SWT.ON);
-	        
-	        graphics.setAlpha(getAlpha());
-	        
-	        graphics.setLineDash(new float[] { (float)(8 * graphics.getAbsoluteScale()), (float)(4 * graphics.getAbsoluteScale()) });
-	        
-	        graphics.setBackgroundColor(getFillColor());
-	        graphics.setForegroundColor(getLineColor());
-	        
-	        Pattern gradient = null;
-	        if(Preferences.STORE.getBoolean(IPreferenceConstants.SHOW_GRADIENT)) {
-	            gradient = FigureUtils.createGradient(graphics, bounds, getFillColor(), getAlpha());
-	            graphics.setBackgroundPattern(gradient);
-	        }
-	        
-	        int type = getDiagramModelObject().getType();
-	        
-	        int[] mainRectangle;
-	        
-	        if(type == 1) {
-	            mainRectangle = new int[] {
-	                    bounds.x, bounds.y,
-	                    bounds.x + bounds.width - 1, bounds.y,
-	                    bounds.x + bounds.width - 1, bounds.y + bounds.height - 1,
-	                    bounds.x, bounds.y + bounds.height - 1
-	            };
-	            
-	            graphics.fillPolygon(mainRectangle);
-	        }
-	        else {
-	            mainRectangle = new int[] {
-	                    bounds.x, bounds.y + TOPBAR_HEIGHT,
-	                    bounds.x + bounds.width - 1, bounds.y + TOPBAR_HEIGHT,
-	                    bounds.x + bounds.width - 1, bounds.y + bounds.height - 1,
-	                    bounds.x, bounds.y + bounds.height - 1
-	            };
-	            
-	            int[] fillShape = new int[] {
-	                    bounds.x, bounds.y,
-	                    (int)(bounds.x + (bounds.width / INSET)), bounds.y,
-	                    (int)(bounds.x + (bounds.width / INSET)), bounds.y + TOPBAR_HEIGHT,
-	                    bounds.getRight().x, bounds.y + TOPBAR_HEIGHT,
-	                    bounds.getRight().x, bounds.getBottom().y,
-	                    bounds.x, bounds.getBottom().y
-	            };
-	            
-	            graphics.fillPolygon(fillShape);
-	            
-	            graphics.setAlpha(getLineAlpha());
-	            graphics.drawLine(bounds.x, bounds.y, bounds.x, bounds.y + TOPBAR_HEIGHT);
-	            graphics.drawLine(bounds.x, bounds.y, (int)(bounds.x + (bounds.width / INSET)), bounds.y);
-	            graphics.drawLine((int)(bounds.x + (bounds.width / INSET)), bounds.y, (int)(bounds.x + (bounds.width / INSET)), bounds.y + TOPBAR_HEIGHT);
-	        }
-	        
-	        if(gradient != null) {
-	            gradient.dispose();
-	        }
+	private static final int TOPBAR_HEIGHT = 18;
+	private static final float INSET = 1.4f;
 
-	        // Outlines
-	        graphics.setAlpha(getLineAlpha());
-	        graphics.drawPolygon(mainRectangle);
-	        
-	        graphics.popState();
-	        drawIconImage(graphics);
-	    }
-	    
-	    @Override
-	    protected Rectangle calculateTextControlBounds() {
-	        Rectangle bounds = getBounds().getCopy();
-	        
-	        int textPosition = ((ITextPosition)getDiagramModelObject()).getTextPosition();
-	        if(textPosition == ITextPosition.TEXT_POSITION_TOP) {
-	            bounds.y -= 3;
-	        }
-	        
-	        return bounds;
-	    }
-	    
-	    /**
-	     * Connection Anchor adjusts for Domain shape
-	     */
-	    class DomainFigureConnectionAnchor extends ChopboxAnchor {
-	        public DomainFigureConnectionAnchor(IFigure owner) {
-	            super(owner);
-	        }
-	        
-	        @Override
-	        public Point getLocation(Point reference) {
-	            Point pt = super.getLocation(reference);
-	            
-	            int type = getDiagramModelObject().getType();
+	public DomainFigure() {
+		super(TEXT_FLOW_CONTROL);
+	}
 
-	            if(type == 1) {
-	                return pt;
-	            }
-	            
-	            Rectangle r = getBox().getCopy();
-	            getOwner().translateToAbsolute(r);
-	            
-	            int shiftY = TOPBAR_HEIGHT - (pt.y - r.y) - 1;
-	            
-	            if(pt.x > r.x + (r.width / INSET) && shiftY > 0) {
-	                pt.y += shiftY;
-	            }
-	            
-	            return pt;
-	        };
-	    }
-	    @Override
-	    public IDiagramModelArchimateObject getDiagramModelObject() {
-	        return (IDiagramModelArchimateObject)super.getDiagramModelObject();
-	    }
+	@Override
+	protected void drawFigure(Graphics graphics) {
+		graphics.pushState();
 
-	    @Override
-	    public ConnectionAnchor getDefaultConnectionAnchor() {
-	        return new DomainFigureConnectionAnchor(this);
-	    }
+		Rectangle bounds = getBounds().getCopy();
 
-	    /**
-	     * Draw the icon image
-	     */
-	    protected void drawIconImage(Graphics graphics) {
-	    	graphics.pushState();
-	        graphics.setForegroundColor(isEnabled() ? ColorConstants.black : ColorConstants.gray); 
-	    	// Start at top right 
-	    	Point pt = getIconOrigin(); 
-	    	
-	    	final Display display = Display.getDefault();
-	        //InputStream path = getClass().getResourceAsStream("img/about.png"); 
-	        
-	        Image imagen = new Image(display, "D:\\domain.png");	    		    	      
-	        int w = imagen.getImageData().width;
-	        int h = imagen.getImageData().height;
-	        graphics.drawImage(imagen, 0, 0, w, h, pt.x, pt.y, 25, 25);
-	        graphics.popState();
-	        imagen.dispose();
-	    }
-	    
-	    /**
-	     * @return The icon start position
-	     */
-	    protected Point getIconOrigin() {
-	        Rectangle bounds = getBounds();
-	        return new Point(bounds.getRight().x - 29, bounds.y + 4);
-	    }
-	    
+		graphics.setAntialias(SWT.ON);
+
+		graphics.setAlpha(getAlpha());
+
+		graphics.setLineDash(
+				new float[] { (float) (8 * graphics.getAbsoluteScale()), (float) (4 * graphics.getAbsoluteScale()) });
+
+		graphics.setBackgroundColor(getFillColor());
+		graphics.setForegroundColor(getLineColor());
+
+		Pattern gradient = null;
+		if (Preferences.STORE.getBoolean(IPreferenceConstants.SHOW_GRADIENT)) {
+			gradient = FigureUtils.createGradient(graphics, bounds, getFillColor(), getAlpha());
+			graphics.setBackgroundPattern(gradient);
+		}
+
+		int type = getDiagramModelObject().getType();
+
+		int[] mainRectangle;
+
+		if (type == 1) {
+			mainRectangle = new int[] { bounds.x, bounds.y, bounds.x + bounds.width - 1, bounds.y,
+					bounds.x + bounds.width - 1, bounds.y + bounds.height - 1, bounds.x, bounds.y + bounds.height - 1 };
+
+			graphics.fillPolygon(mainRectangle);
+		} else {
+			mainRectangle = new int[] { bounds.x, bounds.y + TOPBAR_HEIGHT, bounds.x + bounds.width - 1,
+					bounds.y + TOPBAR_HEIGHT, bounds.x + bounds.width - 1, bounds.y + bounds.height - 1, bounds.x,
+					bounds.y + bounds.height - 1 };
+
+			int[] fillShape = new int[] { bounds.x, bounds.y, (int) (bounds.x + (bounds.width / INSET)), bounds.y,
+					(int) (bounds.x + (bounds.width / INSET)), bounds.y + TOPBAR_HEIGHT, bounds.getRight().x,
+					bounds.y + TOPBAR_HEIGHT, bounds.getRight().x, bounds.getBottom().y, bounds.x,
+					bounds.getBottom().y };
+
+			graphics.fillPolygon(fillShape);
+
+			graphics.setAlpha(getLineAlpha());
+			graphics.drawLine(bounds.x, bounds.y, bounds.x, bounds.y + TOPBAR_HEIGHT);
+			graphics.drawLine(bounds.x, bounds.y, (int) (bounds.x + (bounds.width / INSET)), bounds.y);
+			graphics.drawLine((int) (bounds.x + (bounds.width / INSET)), bounds.y,
+					(int) (bounds.x + (bounds.width / INSET)), bounds.y + TOPBAR_HEIGHT);
+		}
+
+		if (gradient != null) {
+			gradient.dispose();
+		}
+
+		// Outlines
+		graphics.setAlpha(getLineAlpha());
+		graphics.drawPolygon(mainRectangle);
+
+		graphics.popState();
+		drawIconImage(graphics);
+	}
+
+	@Override
+	protected Rectangle calculateTextControlBounds() {
+		Rectangle bounds = getBounds().getCopy();
+
+		int textPosition = ((ITextPosition) getDiagramModelObject()).getTextPosition();
+		if (textPosition == ITextPosition.TEXT_POSITION_TOP) {
+			bounds.y -= 3;
+		}
+
+		return bounds;
+	}
+
+	/**
+	 * Connection Anchor adjusts for Domain shape
+	 */
+	class DomainFigureConnectionAnchor extends ChopboxAnchor {
+		public DomainFigureConnectionAnchor(IFigure owner) {
+			super(owner);
+		}
+
+		@Override
+		public Point getLocation(Point reference) {
+			Point pt = super.getLocation(reference);
+
+			int type = getDiagramModelObject().getType();
+
+			if (type == 1) {
+				return pt;
+			}
+
+			Rectangle r = getBox().getCopy();
+			getOwner().translateToAbsolute(r);
+
+			int shiftY = TOPBAR_HEIGHT - (pt.y - r.y) - 1;
+
+			if (pt.x > r.x + (r.width / INSET) && shiftY > 0) {
+				pt.y += shiftY;
+			}
+
+			return pt;
+		};
+	}
+
+	@Override
+	public IDiagramModelArchimateObject getDiagramModelObject() {
+		return (IDiagramModelArchimateObject) super.getDiagramModelObject();
+	}
+
+	@Override
+	public ConnectionAnchor getDefaultConnectionAnchor() {
+		return new DomainFigureConnectionAnchor(this);
+	}
+
+	/**
+	 * Draw the icon image
+	 */
+	protected void drawIconImage(Graphics graphics) {
+		graphics.pushState();
+		graphics.setForegroundColor(isEnabled() ? ColorConstants.black : ColorConstants.gray);
+		// Start at top right
+		Point pt = getIconOrigin();
+		
+		final File path = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());		
+		final Display display = Display.getDefault();
+		
+		ImageData data = new ImageData(path+ "/" + IArchiImages.ICON_DOMAIN_IMAGE);      		
+		Image image = new Image(display, data);
+		
+		int w = image.getImageData().width;
+		int h = image.getImageData().height;
+		graphics.drawImage(image, 0, 0, w, h, pt.x, pt.y, 25, 25);
+		graphics.popState();
+		image.dispose();
+	}
+
+	/**
+	 * @return The icon start position
+	 */
+	protected Point getIconOrigin() {
+		Rectangle bounds = getBounds();
+		return new Point(bounds.getRight().x - 29, bounds.y + 4);
+	}
+
 }
--- com.archimatetool.editor/src/com/archimatetool/editor/ui/IArchiImages.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/ui/IArchiImages.java
@@ -87,7 +87,7 @@ public interface IArchiImages {
     String ICON_GROUPING = ARCHIMATE_IMGPATH + "grouping.png"; //$NON-NLS-1$
     
     String ICON_DOMAIN = ARCHIMATE_IMGPATH + "domain.png"; //$NON-NLS-1$
-
+    String ICON_DOMAIN_IMAGE = ARCHIMATE_IMGPATH + "domain@2x.png"; //$NON-NLS-1$
     
     String ICON_IMPLEMENTATION_EVENT = ARCHIMATE_IMGPATH + "implementation-event.png"; //$NON-NLS-1$
     String ICON_LOCATION = ARCHIMATE_IMGPATH + "location.png"; //$NON-NLS-1$
